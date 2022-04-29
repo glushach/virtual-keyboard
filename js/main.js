8f79cfcdc583ee3b7240efd3d4be67ddbb5eca53
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-04-29 19:20:18 +0300

    feat: add html and style for project

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@ const foo = 'string';
 console.log(data);
 console.log('\\')
 
-document.querySelector('body').innerHTML = `
-<p>${data.en['\\']}</p>
-`
+
 
 // npx eslint js/main.js - чтобы проверить на соответсвие из консоли
 // npx eslint js/main.js --fix - автоматически из консоли фиксить ошибки в данном файле
